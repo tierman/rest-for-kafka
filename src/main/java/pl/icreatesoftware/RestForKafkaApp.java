1bc7c3cbf3fc14edd38b097965d12eb157371ca8
Tomek Jonczyk <tomek.jonczyk@gmail.com>
2022-07-15 23:14:05 +0200

    Simple runnable config.

---
+++ src/main/java/pl/icreatesoftware/RestForKafkaApp.java
@@ -0,0 +1,12 @@
+package pl.icreatesoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestForKafkaApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestForKafkaApp.class, args);
+    }
+}
