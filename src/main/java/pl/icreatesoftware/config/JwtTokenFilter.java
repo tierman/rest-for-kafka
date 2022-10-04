aee904f6174bb3938f970b88f12b85750a4a3f33
Tomek Jonczyk <tomek.jonczyk@gmail.com>
2022-10-04 14:35:20 +0200

    Refactor.

---
+++ src/main/java/pl/icreatesoftware/config/JwtTokenFilter.java
@@ -0,0 +1,44 @@
+package pl.icreatesoftware.config;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static org.apache.commons.lang3.StringUtils.isEmpty;
+
+@Component
+class JwtTokenFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        final String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+        String sessionId = request.getSession().getId();
+        if (!sessionId.isBlank()) {
+            
+        }
+
+        if (isEmpty(header) || !header.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // Get jwt token and validate
+        final String token = header.split(" ")[1].trim();
+        //if (!jwtTokenUtil.validate(token)) {
+        //    chain.doFilter(request, response);
+         //   return;
+        //}
+
+        // Get user identity and set it on the spring security context
+    }
+}
---
+++ src/main/java/pl/icreatesoftware/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package pl.icreatesoftware.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+
+    private final JwtTokenFilter jwtTokenFilter;
+
+    SecurityConfig(JwtTokenFilter jwtTokenFilter) {
+        this.jwtTokenFilter = jwtTokenFilter;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        //.anyRequest().permitAll()
+                        //.antMatchers("/", "/error", "/swagger-ui/").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login();
+        http
+                .cors()
+                .and()
+                .csrf().disable();
+
+        // Add JWT token filter
+        http.addFilterBefore(
+                jwtTokenFilter,
+                UsernamePasswordAuthenticationFilter.class
+        );
+        return http.build();
+    }
+}
